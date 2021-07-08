998188fb46c9ae9e3bd60f9c7bb1d4fa333ad9cf
Sethhhhhhh <yohannvvt@gmail.com>
2021-07-08 21:47:53 +0200

    module_cpp_00: ex00: done.

---
+++ cpp_module_00/ex00/megaphone.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main(int ac, char **av)
+{
+    if (ac <= 1) {
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+        return (0);
+    } else {
+        for (unsigned int i = 1; i < ac; i++) {
+            for (unsigned int j = 0; av[i][j]; j++)
+                av[i][j] = toupper(av[i][j]);
+            std::cout << av[i];
+        }
+        std::cout << std::endl;
+    }
+}
