0591613e670c540670a115e16715b59833ce34db
Sethhhhhhh <yohannvvt@gmail.com>
2021-07-12 18:56:49 +0200

    cpp_module: cpp_module_01: ex02: done.

---
+++ cpp_module_01/ex02/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+int main(void) {
+	std::string		string = "HI THIS BRAIN";
+	std::string*	stringPTR = &string;
+	std::string&	stringREF = string;
+	
+	std::cout << *stringPTR << std::endl;
+	std::cout << stringREF << std::endl;
+
+	return (0);
+}
