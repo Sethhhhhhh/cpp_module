a53ba8bd71219ae7328dfa28466832b9c720385f
Sethhhhhhh <yohannvvt@gmail.com>
2021-07-15 13:33:12 +0200

    cpp_module_02: ex00: fuuuuuccck

--- cpp_module_02/ex00/Fixed.hpp
+++ cpp_module_02/ex00/Fixed.hpp
@@ -1,13 +1,23 @@
 #ifndef __FIXE_HPP__
 #define __FIXE_HPP__
 
+#include <iostream>
+
 class Fixed {
 
-private:
-
 public:
-    Fixed(void);
-    ~Fixed();
+	Fixed(void);
+	~Fixed();
+	Fixed(Fixed const & src);
+
+	Fixed &	operator=(Fixed const & src);
+
+	int		getRawBits(void) const;
+	void	setRawBits(int const raw);
+
+private:
+	int					_n;
+	static int const	_bits = 8;
 
 };
 
