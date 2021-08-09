2b6e9b4e0cb2c230b5b724979e68e55192c9d47a
Sethhhhhhh <yohannvvt@gmail.com>
2021-08-09 20:40:27 +0200

    cpp_module_07: ex00: after 6 hours of FUCKING (but great) 42 VIDEO it's done.

---
+++ cpp_module_07/ex00/whatever.hpp
@@ -0,0 +1,18 @@
+template<typename T>
+void    swap(T & a, T & b) {
+    T c = b;
+    b = a;
+    a = c;
+    
+    return ;
+}
+
+template<typename T>
+T & min(T & a, T & b) {
+    return ((a > b ? b : a));
+}
+
+template<typename T>
+T & max(T & a, T & b) {
+    return ((a > b ? a : b));
+}
