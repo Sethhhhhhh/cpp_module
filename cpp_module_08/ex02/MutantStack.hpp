a49996ec1f745d857c0fd6e1c063585e5aeb3ce0
Sethhhhhhh <yohannvvt@gmail.com>
2021-08-11 16:35:42 +0200

    cpp_module_08: done.

---
+++ cpp_module_08/ex02/MutantStack.hpp
@@ -0,0 +1,27 @@
+#ifndef __MUTANT_STACK_HPP__
+#define __MUTANT_STACK_HPP__
+
+#include <stack>
+
+template<typename T>
+class MutantStack : public std::stack<T> {
+
+    public:
+        typedef typename    std::stack<T>::container_type::iterator iterator;
+
+        MutantStack(void) {};
+        MutantStack(const MutantStack<T> & src) {};
+        ~MutantStack(void) {};
+
+        MutantStack &   operator=(const MutantStack<T> & src) {
+            return (*this);
+        };
+        iterator    begin(void) {
+            return (this->c.begin());
+        };
+        iterator    end(void) {
+            return (this->c.end());
+        };
+};
+
+#endif
